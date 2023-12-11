5fe2c0bf818d49bbdfa978198d57440c4ec46b30
Zahra HAMDANE <zahra.hamdane2023@campus-eni.fr>
2023-12-11 10:30:53 +0100

    Inisialisation du projet

---
+++ src/main/java/fr/eni/pizzaonline/PizzaOnLine/PizzaOnLineApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.pizzaonline.PizzaOnLine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOnLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOnLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/pizzaonline/PizzaOnLine/PizzaOnLineApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.pizzaonline.PizzaOnLine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaOnLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
